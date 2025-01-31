3fc1f16f85eb45994ba52df332c4d13b954bdcde
thpauken <thpauken@hotmail.com>
2025-01-31 14:16:45 +0000

    first commit, added test files

---
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -0,0 +1,5 @@
+		namespace FactorialService.Tests; 
+        using FactorialLibrary; 
+        public class FactorialServiceTests { 
+            [Fact] public void Test1() { var result = FactorialService.Factorial(0); Assert.Equal(1,result); } 
+            }
